e0dab049cbc60d0c6cb87a83e38659f3db8d9dd5
Jaume Vila <jaume.vila@uvic.cat>
2014-03-10 15:43:20 +0100

    Initial commit

---
+++ GoogleMapsTable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GoogleMapsTable
+//
+//  Created by ioas on 10/03/14.
+//  Copyright (c) 2014 ioas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GoogleMapsTable/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GoogleMapsTable
+//
+//  Created by ioas on 10/03/14.
+//  Copyright (c) 2014 ioas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
